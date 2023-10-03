9c61bdcd7035faddc25c230996db1e881dcc8e1b
Evgennna <138823629+Evgennna@users.noreply.github.com>
2023-10-04 00:06:31 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,58 +1,40 @@
-from testpage import OperationHelper
-import logging
-import time
-
 import yaml
+import time
+from BaseApp import BasePage
+from testpage import Operations
 
 with open("testdata.yaml") as f:
-    data = yaml.safe_load(f)
-    name = data["username"]
-    paswd = data["password"]
+    testdata = yaml.safe_load(f)
+
+name = testdata["username"]
+passwd = testdata["password"]
 
 
-def test_step1(browser):
-    logging.info("Test1 Starting")
-    testpage = OperationHelper(browser)
-    testpage.go_to_site()
-    testpage.enter_login("test")
-    testpage.enter_pass("test")
-    testpage.click_login_button()
+def test_step1(err_401, browser):
+    site = BasePage(browser)
+    site.go_to_site()
+    page = Operations(browser)
+    page.enter_bad_login()
+    page.enter_bad_pass()
+    page.click_login_button()
+    assert page.get_error_text() == err_401
+
+
+def test_step2(hello_user, browser, alert_text):
+    site = BasePage(browser)
+    site.go_to_site()
+    page = Operations(browser)
+    page.enter_good_login()
+    page.enter_good_pass()
+    page.click_login_button()
+    # SendMail()
+    assert page.get_hello_user() == hello_user
+    page.click_contact_button()
     time.sleep(3)
-    assert testpage.get_error_text() == "401", "Test_1 FAIL"
-
-
-def test_step2(browser):
-    logging.info("Test2 Starting")
-    testpage = OperationHelper(browser)
-    testpage.go_to_site()
-    testpage.enter_login(name)
-    testpage.enter_pass(paswd)
-    testpage.click_login_button()
-    time.sleep(2)
-    assert testpage.get_profile_text() == f"Hello, {name}", "Test_2 FAIL"
-
-
-def test_step3(browser):
-    logging.info("Test3 Starting")
-    testpage = OperationHelper(browser)
-    testpage.click_to_do_new_post()
-    testpage.enter_title("Hello, world")
-    testpage.enter_description("help")
-    testpage.enter_content("Bye, world")
-    testpage.click_save_post_button()
-    time.sleep(5)
-    assert testpage.get_title_text() == "Hello, world", "Test_3 FAIL"
-
-
-
-
-def test_step4(browser):
-    logging.info("Test4 Starting")
-    testpage = OperationHelper(browser)
-    testpage.click_contact_button()
-    testpage.enter_name("Eva")
-    testpage.enter_email("any@mail.ru")
-    testpage.enter_contact_content("my contact")
-    testpage.contact_us_save_button()
+    page.enter_name()
+    page.enter_email()
+    page.enter_content()
     time.sleep(3)
-    assert testpage.get_alert_text() == "Form successfully submitted", "Test_4 FAIL"
+    page.click_contact_us_button()
+    time.sleep(3)
+    assert page.alert() == alert_text
